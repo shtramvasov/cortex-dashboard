d575f45a58bf20f31d6a2840c72e328689d884b0
IrishAir <bls21@mail.ru>
2022-07-07 13:31:42 +0500

    added login route

--- server/jwtAuthentication.js
+++ server/jwtAuthentication.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('./database');
+const jwtGenerator = require('./jwtGenerator');
 
 // register
 router.post('/registration', async (req, res) => {
@@ -13,7 +14,33 @@ router.post('/registration', async (req, res) => {
     }
     const newUser = await pool.query
       ('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [username, password]);
-      res.json(newUser.rows[0]);
+    const token = jwtGenerator(newUser.rows[0].user_id);
+    res.json({ token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+
+// login
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await pool.query
+      ('SELECT * FROM users WHERE username = $1', [username]);
+    if (user.rows.length === 0) {
+       return res.status(401).json('Пользователь не найден');
+    }
+    let validPassword = false;
+    if(password === user.rows[0].password) {
+      validPassword = true;
+    }
+    if (!validPassword) {
+      return res.status(401).json('Неверный пароль');
+    }
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json({ token });
   } catch (err) {
     console.error(err.message);
     res.status(500).send(err.message);
