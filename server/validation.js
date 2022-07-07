e21b3439af1ce487bdf5a23754193e832e33a796
IrishAir <bls21@mail.ru>
2022-07-07 16:36:30 +0500

    validation token middleware

---
+++ server/validation.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = function(req, res, next) {
+  try {
+    const token = req.header('token');
+    if (!token) {
+      return res.status(403).json('Неверный токен');
+    }
+    const payload = jwt.verify(token, process.env.jwtSecret);
+    req.user = payload.user;
+  } catch (err) {
+    res.status(403).json('Неверный токен');
+  }
+};
