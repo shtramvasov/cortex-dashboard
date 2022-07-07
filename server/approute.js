2398a5e18cdf145b57d2caf299b119cab28cdfc7
IrishAir <bls21@mail.ru>
2022-07-07 17:10:45 +0500

    added app route middleware

---
+++ server/approute.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const pool = require('./database');
+const validation = require('./validation');
+
+router.get('/', validation, async (req, res) => {
+  try {
+    // res.json(req.user)
+    const user = await pool.query
+      ('SELECT username FROM users WHERE id = $1', [req.user]);
+    res.json(user.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json('Ошибка доступа');
+  }
+});
+
+module.exports = router;
