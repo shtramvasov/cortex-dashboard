2cedfde3606c51813ee50bcac2cde1a40d6c0083
IrishAir <bls21@mail.ru>
2022-07-08 09:33:42 +0500

    token validation on refresh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
 import LoginPage from './pages/LoginPage';
@@ -7,6 +7,22 @@ import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const checkAuthenticated = async () => {
+		try {
+			const res = await fetch('http://localhost:5000/validate', {
+				method: 'GET',
+				headers: { token: localStorage.token },
+			});
+			const parseRes = await res.json();
+			parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
+	useEffect(() => {
+		checkAuthenticated();
+	}, []);
 
 	const setAuth = boolean => {
 		setIsAuthenticated(boolean);
